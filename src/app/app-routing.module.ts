8005f97d0ba9f2eec4adf5b2863d8be7a09ff8d1
kishor.shetti <kishor.shetti@shauryatechnosoft.com>
2025-07-30 10:04:37 +0530

    app routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './layouts/public/public.component';
+import { SecureComponent } from './layouts/secure/secure.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'temp', pathMatch: 'full' },
+  {
+    path: '', component: PublicComponent,
+    loadChildren: () => import('./layouts/public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path: '',
+    component: SecureComponent,
+    loadChildren: () => import('./layouts/secure/secure.module').then(m => m.SecureModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
