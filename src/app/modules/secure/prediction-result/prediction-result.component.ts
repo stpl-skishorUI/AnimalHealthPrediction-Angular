a10a21f997acc4fd2525ecae510300557bcb3631
kishor.shetti <kishor.shetti@shauryatechnosoft.com>
2025-07-30 17:29:41 +0530

    feat: add prediction result component with template, styles, and tests

---
+++ src/app/modules/secure/prediction-result/prediction-result.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-prediction-result',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './prediction-result.component.html',
+  styleUrls: ['./prediction-result.component.scss']
+})
+export class PredictionResultComponent {
+
+}
