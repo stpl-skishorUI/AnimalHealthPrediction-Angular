8731948a98e9f57694aefbcf7bdd99e64ce0ca71
kishor.shetti <kishor.shetti@shauryatechnosoft.com>
2025-07-30 17:29:34 +0530

    fix: add missing route for prediction result component in secure routing

--- src/app/layouts/secure/secure-routing.module.ts
+++ src/app/layouts/secure/secure-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { SecureComponent } from './secure.component';
 import { Routes, RouterModule } from '@angular/router';
 import { PredictionFormComponent } from '../../modules/secure/prediction-form/prediction-form.component';
+import { PredictionResultComponent } from 'src/app/modules/secure/prediction-result/prediction-result.component';
 
 const routes: Routes = [
   { path: '', component: SecureComponent },
   { path: 'dashboard', loadChildren: () => import('../../modules/secure/dashboard/dashboard.module').then(m => m.DashboardModule), data: { breadcrumb: [{ title: 'Dashboard', active: true }] }, },
   { path: 'prediction-form', component: PredictionFormComponent, data: { breadcrumb: [{ title: 'Prediction Form', active: true }] }, },
+  { path: 'prediction-result', component: PredictionResultComponent, data: { breadcrumb: [{ title: 'Prediction Result', active: true }] }, },
 ];
 
 @NgModule({
