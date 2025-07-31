691b1aa68cacb643f47803ed17cba19d12ff96ed
kishor.shetti <kishor.shetti@shauryatechnosoft.com>
2025-07-31 16:40:11 +0530

    feat: update prediction result component to use routerLink for navigation and include RouterModule in imports

--- src/app/modules/secure/prediction-result/prediction-result.component.ts
+++ src/app/modules/secure/prediction-result/prediction-result.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-prediction-result',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './prediction-result.component.html',
   styleUrls: ['./prediction-result.component.scss']
 })
